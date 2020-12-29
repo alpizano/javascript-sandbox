e7e30e1c3ecca87dc7b8a50f8c6b7ae030ffed64
alpizano <apizano@purdue.edu>
2020-12-28 20:54:14 -0600

    add data types

---
+++ 12_data_types.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// Uncaught ReferenceError because of Strict Mode
+//randomThing = 42;
+let anotherThing = 69;
+let javascriptIsFun = true;
+
+//console.log(typeof randomThing);
+console.log(typeof anotherThing);
+console.log(javascriptIsFun);
+
+const stuff = "Hello";
+console.log(stuff);
+
+// i.e. Dynamic typing; boolean => string
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+
+// empty variable is undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+
+// won't compile because attempting to mutate constant
+//stuff = "Why";
+//console.log(stuff);
+
+// type of null is object; error, should be Null
+console.log(typeof null);
