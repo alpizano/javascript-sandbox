346cc2a0ff2cf91bc52942b03d8f4acc36b2a74e
alpizano <apizano@purdue.edu>
2020-12-28 20:27:05 -0600

    this theory

---
+++ 97_this.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// function expression
+const calcAgeExpression = function (birthYear) {
+  // "this" is undefined
+  console.log(this);
+  return 2020 - birthYear;
+};
+
+calcAgeExpression(1987);
+
+const calcAgeArrow = (birthYear) => {
+  // "this" is the parent/Lexical Window object
+  console.log(this);
+  return 2020 - birthYear;
+};
+
+calcAgeArrow(1987);
+
+const al = {
+  year: 1987,
+  calcAge: function () {
+    console.log(this);
+    // "year" throws Uncaught ReferenceError; needs this.year
+    return 2020 - this.year;
+  },
+};
+
+al.calcAge();
