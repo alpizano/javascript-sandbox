cc4921b963f18b08d703cb392c4c740823706efa
alpizano <apizano@purdue.edu>
2020-12-30 02:37:28 -0600

    return bills array

---
+++ 3_coding_challenge_2.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let bills = [275, 40, 430];
+
+function calcTip(bills) {
+  let totals = [];
+
+  bills.forEach((element) => {
+    let tip = 300 >= element && element >= 50 ? element * 0.15 : element * 0.2;
+    console.log(
+      `For $${element} bill, tip is $${tip} for a total value of $${
+        element + tip
+      }`
+    );
+    totals.push(element + tip);
+  });
+  return totals;
+}
+
+const totals = calcTip(bills);
+console.log(totals);
