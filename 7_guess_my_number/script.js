7142a9f6174419f86f3abbb399331feead3f240e
alpizano <apizano@purdue.edu>
2020-12-30 19:12:45 -0600

    get DOM elements

--- 7_guess_my_number/script.js
+++ 7_guess_my_number/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Selecting class
+console.log(document.querySelector('.message'));
+
+// Get text from dom element
+console.log(document.querySelector('.message').textContent);
+
+// Selecting # id; returns null because doesn't exist
+console.log(document.querySelector('#message'));
