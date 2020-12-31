1882281cb036f715c790d9b788bc66f5cd9e5565
alpizano <apizano@purdue.edu>
2020-12-31 17:27:53 -0600

    add GET neighbor country logic

--- 16_async_js.js
+++ 16_async_js.js
@@ -4,9 +4,30 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 console.log(`CountriesContainer outside of function: ${countriesContainer}`);
 
-function getCountries(country) {
-  const request = new XMLHttpRequest();
+const renderCountry = (data, className = "") => {
+  const html = `<article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${data.population}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].code}</p>
+    </div>
+  </article>`;
 
+  console.log(`CountriesContainer inside of function: ${countriesContainer}`);
+
+  // doesn't work
+  // countriesContainer.insertAdjacentHTML("beforeend", html);
+  // countriesContainer.style.opacity = 1;
+
+  document.querySelector(".countries").insertAdjacentHTML("beforeend", html);
+  document.querySelector(".countries").style.opacity = 1;
+};
+
+const getCountryAndNeighbor = (country) => {
+  let request = new XMLHttpRequest();
   request.open("GET", `https://restcountries.eu/rest/v2/name/${country}`);
   request.send();
 
@@ -17,29 +38,29 @@ function getCountries(country) {
 
     // Parse as JSON
     // Destructure array
-    const [data] = JSON.parse(this.responseText);
+    let [data] = JSON.parse(this.responseText);
     console.log(data);
 
-    const html = `<article class="country">
-  <img class="country__img" src="${data.flag}" />
-  <div class="country__data">
-    <h3 class="country__name">${data.name}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${data.population}</p>
-    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-    <p class="country__row"><span>💰</span>${data.currencies[0].code}</p>
-  </div>
-</article>`;
+    // Render country 1
+    renderCountry(data);
 
-    console.log(`CountriesContainer inside of function: ${countriesContainer}`);
+    // Get neighbor country 2 (array)
+    const [neighbour] = data.borders;
 
-    // doesn't work
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    // document.querySelector(".countries").insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
-    // document.querySelector(".countries").style.opacity = 1;
+    if (!neighbour) return;
+
+    request = new XMLHttpRequest();
+    request.open("GET", `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    request.send();
+
+    request.addEventListener("load", function () {
+      data = JSON.parse(this.responseText);
+      console.log(data);
+
+      renderCountry(data, "neighbour");
+    });
   });
-}
+};
 
-getCountries("usa");
-getCountries("portugal");
+getCountryAndNeighbor("usa");
+//getCountryAndNeighbor("portugal");
