ec7ce5adb5d226641a05cf246a1c0915d719930f
alpizano <apizano@purdue.edu>
2021-01-03 01:05:33 -0600

    code refactor

--- 7_dom_events_real/project_2/script.js
+++ 7_dom_events_real/project_2/script.js
@@ -5,16 +5,20 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-btnsOpenModal.forEach((element) => {
-  element.addEventListener("click", function () {
-    console.log(element.textContent);
-
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
-});
-
-btnCloseModal.addEventListener("click", function () {
+const hide = () => {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+};
+
+const show = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+btnsOpenModal.forEach((element) => {
+  element.addEventListener("click", show);
 });
+
+btnCloseModal.addEventListener("click", hide);
+
+overlay.addEventListener("click", hide);
