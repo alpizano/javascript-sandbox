cf4f793f458e1ffa56c56265fa76e12880a1a0f6
alpizano <apizano@purdue.edu>
2021-01-03 01:51:03 -0600

    hide dice and scores

--- 7_dom_events_real/pig_game/script.js
+++ 7_dom_events_real/pig_game/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+// put scores to 0
+// make dice dissapear
+
+const score = document.querySelectorAll(".score");
+const dice = document.querySelector(".dice");
+
+const score0 = document.querySelector("#score--0");
+const score1 = document.getElementById("score--1");
+
+console.log("loading....");
+
+score.forEach((element) => (element.textContent = 0));
+
+// use hidden class instead of manually setting CSS
+//dice.style.display = "none";
+dice.classList.add("hidden");
