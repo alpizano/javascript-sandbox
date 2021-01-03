620d9e6f99f323a0a9bfc98c08eeb62146e00c2e
alpizano <apizano@purdue.edu>
2021-01-02 18:08:49 -0600

    map sets challenge 3

--- 9_challenge_3.js
+++ 9_challenge_3.js
@@ -14,12 +14,34 @@ const gameEvents = new Map([
   [92, "🔶 Yellow card"],
 ]);
 
+// Task 1
 // convert map to array
 const events = [...gameEvents.values()];
 // convert array to set then back to array
 console.log(events);
-console.log(new Set(events));
+const eventsSet = new Set(events);
+//console.log(eventsSet.size);
 
 const eventsNoDups = [...new Set(events)];
 
 console.log(eventsNoDups);
+
+// Task 2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// Task 3
+console.log(gameEvents.size);
+const message = `An event happened, on average, every ${
+  90 / gameEvents.size
+} minutes`;
+console.log(message);
+
+// Task 4
+for (const [key, val] of gameEvents) {
+  if (key > 45) {
+    console.log(`[SECOND HALF] ${key}: ${val}`);
+  } else {
+    console.log(`[FIRST HALF] ${key}: ${val}`);
+  }
+}
