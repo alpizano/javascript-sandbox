c1b1348a8a0713c07f3dc26e4a6f0cacadc69b1b
alpizano <apizano@purdue.edu>
2021-01-03 00:53:54 -0600

    add forEach for event listeners on modal buttons

---
+++ 7_dom_events_real/project_2/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+btnsOpenModal.forEach((element) => {
+  element.addEventListener("click", function () {
+    console.log(element.textContent);
+  });
+});
