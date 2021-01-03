bd4148bca189a003ad16ca68eb35e5c8335330b1
alpizano <apizano@purdue.edu>
2021-01-02 19:54:16 -0600

    call, apply and bind

--- 10_functs.js
+++ 10_functs.js
@@ -33,10 +33,29 @@ lufthansa.book(635, "John Doe");
 console.log(lufthansa);
 
 const eurowings = {
-  name: "Eurowings",
+  airline: "Eurowings",
   iataCode: "EW",
   bookings: [],
 };
 
 // store function in variable
 const book = lufthansa.book;
+
+// TypeError: Cannot read property of 'airline' of undefined
+//book(23, "Sarah Williams");
+
+// bind "this" to eurowings
+book.call(eurowings, 23, "Sarah Williams");
+
+// apply method
+const args = [44, "Johnny Boy"];
+book.apply(eurowings, args);
+
+const functGenerator = (rate) => {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const newFunct = functGenerator(0.1);
+console.log(newFunct(200));
