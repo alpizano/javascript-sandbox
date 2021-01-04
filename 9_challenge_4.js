0025afad7d964f319f7c9316cf84a96b8baec291
alpizano <apizano@purdue.edu>
2021-01-04 04:31:50 -0600

    completed algorithm for camel case

--- 9_challenge_4.js
+++ 9_challenge_4.js
@@ -1,11 +1,18 @@
 "use strict";
 
+// DATA
+// underscore_case
+// first_name
+// Some_Variable
+// caculate_AGE
+// delayed_departure
+
 const txtElement = document.createElement("textarea");
 txtElement.className = "form-control";
 
 const btnElement = document.createElement("button");
-btnElement.className = "btn btn-primary";
-btnElement.innerHTML = "9_CHALLENGE_4";
+btnElement.className = "btn btn-primary myButton";
+btnElement.innerHTML = "CLICK ME";
 
 //document.body.append(txtElement);
 //document.body.append(btnElement);
@@ -13,4 +20,24 @@ btnElement.innerHTML = "9_CHALLENGE_4";
 document.querySelector(".xs-12").insertAdjacentElement("beforeend", txtElement);
 document.querySelector(".xs-12").insertAdjacentElement("beforeend", btnElement);
 
-const text = document.querySelector("textarea").value;
+document.querySelector(".myButton").addEventListener("click", function () {
+  const text = document.querySelector("textarea").value;
+  console.log(text);
+
+  const textArray = text.split("\n");
+  console.log(textArray);
+
+  for (const word of textArray) {
+    const _index = word.indexOf("_");
+    const segment = word.slice(_index, _index + 2);
+    const newWord = word
+      .replace(segment, word[_index + 1].toUpperCase())
+      .replace(
+        word.slice(word[0], _index),
+        word.slice(word[0], _index).toLowerCase()
+      )
+      .replace(word.slice(_index + 2), word.slice(_index + 2).toLowerCase());
+
+    console.log(newWord);
+  }
+});
